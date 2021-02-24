23a4783a00495e874661e9352ce511e47c5a5faf
aleperz <alejandraperez.659@gmail.com>
2021-02-24 17:24:11 -0500

    actualizando funcion validation.isValid

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-let cardNumber = document.getElementById("card_number").value;
+let cardNumber;
 
+// al darle click en el boton validar, se ejecuta la función isValid del objeto validator, quien regresa false para tarjeta invalida y true para tarjeta valida. El retorno quedara en la variable validacion
 
-document.getElementById("buttonValidar").addEventListener("click", function(){
-    console.log(cardNumber)
-    validator.isValid(cardNumber)     
+document.getElementById('card_number').addEventListener('change', (event) => {
+    cardNumber = event.target.value
+})
+
+document.getElementById("buttonValidar").addEventListener("click", function () {
+  
+  console.log(cardNumber)
+  
+  console.log(validator.isValid(cardNumber));
+  const isValid = validator.isValid(cardNumber);
+  showMessage(isValid);
 });
 
+function showMessage(resultValidation) {
+    const resultHTML = document.getElementById('resultvalid');
+  let number_mask = validator.maskify(
+    cardNumber
+  );
+  if (resultValidation) {
+    resultHTML.innerHTML = `Su tarjeta N° ${number_mask} es valida`;
+  } else {
+    document.getElementById(
+        resultHTML.innerHTML = `Su tarjeta N° ${number_mask} es invalida`;
+  }
+}
 console.log(validator);
-/* validator.maskify(cardNumber); */
-
-
-
--- src/validator.js
+++ src/validator.js
@@ -1,44 +1,44 @@
-const validator = {
+let validator = {
 
-  isValid: function(cardnumber){
-    let char = cardnumber.split("").map(Number);
-     /* console.log(char);
-     console.log(typeof char[0]); */
+  isValid: function(cardnum){
+      //divide el numero por digitos y lo almacena en un array de strings, con map lo transforma en array de numeros
+    let char = cardnum.split("").map(Number);
+     //reversa el array char
      let charReverse = char.reverse();
-
+       //busca las posiciones pares (index impar) del array y lo multiplica x 2
      for (var i=0; i< charReverse.length; i++){
       if(i % 2 !== 0){
           charReverse[i] = charReverse[i] * 2
-          // condicional para separar y sumar los digitos del valor si son mayor a 10 
-          if(charReverse[i] >= 10){
+          // condicional para separar (split) y sumar (reduce) los digitos convertidos en numero (map) del valor si son mayor a igual a 10 
+          if(charReverse[i] >= 10){              
               charReverse[i] = String(charReverse[i]).split("").map(Number).reduce(function(a,b){return a + b});                
           } 
       }       
       else{
-          charReverse[i] = charReverse[i] * 1            
+          charReverse[i] = charReverse[i] * 1;            
       }        
   }
-  /* console.log(char); */
+  
   let valid = charReverse.reduce(function(a,b){return a + b});
-  /* console.log(valid);  */
+  
   if(valid % 10 == 0){
-      /* console.log("tarjeta valida") */
+     //tarjeta valida
       return true;
   }
   else{
-      /* console.log("tarjeta invalida") */
+      //tarjeta invalida
       return false;
   }  
     
-}
+},
 
- /*  maskify: function(num,enm){
-    if(enm.length >= 5){
+   maskify: function(cardnum){
+    if(cardnum.length >= 5){
       const regexp = /.(?=.{4})/g;
       const substr = "#";
-      document.getElementById("replace").innerHTML  = `Su tarjeta N° ${enm.replace(regexp,substr)} es undefined`;
+      return cardnum.replace(regexp,substr)}
   }
-} */
-}
+} 
+
 
 export default validator;
